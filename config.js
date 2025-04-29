6967fe859ba10c19a5369dda8d59191fc7e64358
MaTierra9 <56006411+MaTierra9@users.noreply.github.com>
2025-04-28 21:03:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -16,12 +16,51 @@ var config = {
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: 'Rancho Palma: Un agujero negro de la Operación Bastión',
-    subtitle: 'Dos ranchos asegurados en la operación Bastión ya habían sido cateados años atrás, pero se presentaron como recientes. Autotidades mexiquenses y federales no brindaron explicaciones al respecto.',
+    subtitle: 'Dos ranchos asegurados en la operación Bastión ya habían sido cateados años atrás; pero se presentaron como recientes. Autoridades mexiquenses y federales no brindaron explicaciones al respecto.',
     byline: 'Texto y visualización: Ivan Ortiz',
     footer: '<br> Esta visualización fue creada usando la plantilla <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a>.',
     chapters: [
         {
             id: 'slug-style-id',
+            alignment: 'full',
+            hidden: false,
+            title: '',
+            image: 'https://adnoticias.mx/wp-content/uploads/2024/12/AD-Noticias-35-anos.svg',
+            description: 'A finales de marzo de 2025 autoridades estatales y federales realizaron la Operación Bastión, una serie de cateos en 21 inmuebles presuntamente utilizados por la Nueva Familia Michoacana en el Estado de México. Sin embargo, dos de de estos ranchos, Calpa y Las Piñuelas, fueron asegurados en 2020 y 2022. Así lo revelan informes albergados los Guacamaya Leaks localizados por AD Noticias. Ni la Fiscalía ni el Gobierno Mexiquense ni el Ejército ',
+            location: {
+                center: [-99.74307, 19.46924],
+                zoom: 7.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                     layer: 'MunicipiosEdomex',
+                     opacity: 1,
+                     duration: 5000
+                },
+                {
+                     layer: 'ranchos-opb',
+                     opacity: 1,
+                     duration: 5000
+                }
+            ],
+            onChapterExit: [
+                {
+                     layer: 'MunicipiosEdomex',
+                     opacity: 0
+                 }, 
+                 {
+                     layer: 'ranchos-opb',
+                     opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'primer-identificador',
             alignment: 'full',
             hidden: true,
             title: '',
@@ -55,20 +94,19 @@ var config = {
                  }, 
                  {
                      layer: 'ranchos-opb',
-                     opacity: 1,
-                     duration: 5000
+                     opacity: 0
                 }
             ]
         },
         {
-            id: 'second-identifier',
+            id: 'segundp-identificador',
             alignment: 'right',
             hidden: false,
             title: 'Washington, D.C.',
             image: './assets/washington-dc.jpg',
             description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
             location: {
-                center: [-99.74307, 19.46924],
+                center: [-100.28560, 18.79171],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: -43.2,
